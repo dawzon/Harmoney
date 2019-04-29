a3b1de692958e9132e3fd9bb46ff48b56c548e4f
sirdude103 <sirdude103@gmail.com>
2019-04-29 00:08:00 -0500

    Added rudimentary log-in page (no functionality)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ExpensesComponent } from './expenses/expenses.component'
+import { LoginComponent } from './login/login.component'
 import { VisualizationComponent } from './visualization/visualization.component';
 
 const routes: Routes = [
   { path: 'expenses', component: ExpensesComponent},
-  { path: 'visualization', component: VisualizationComponent}
+  { path: 'visualization', component: VisualizationComponent},
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
