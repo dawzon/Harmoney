221415c841b492a32d34d0d00dcb0eb9e654277f
keenanlk <keenanlkaufman@gmail.com>
2019-05-06 22:58:14 -0500

    visualization connects to database

--- src/app/visualization/visualization.component.ts
+++ src/app/visualization/visualization.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Expenses } from '../models/user';
+
 
 
 @Component({
@@ -8,22 +11,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizationComponent implements OnInit {
 
+  expenses: Expenses[];
+
   public barChartOptions={
     scaleShowVerticleLines: false,
     responsive: true
   };
+  grocSum: number = 0;
+  utilSum: number = 0;
+  enteSum: number = 0;
+  rentSum: number = 0;
+  clotSum: number = 0;
+  healSum: number = 0;
 
+  //get sums
+  
+  //chart
   public barChartLabels = ['Groceries', 'Utilities', 'Entertainment', 'Rent', 'Clothing', 'Health'];
   public barChartType = 'bar';
   public barChartLegent = true;
-  public barChartData = [
-    {data: [65, 59, 30, 350, 55, 40], label: 'All time Expenses'},
+  public barChartData = [];
     
-  ]
 
-  constructor() { }
-
+  constructor(private dataService: DataService) { }
+  //get data
   ngOnInit() {
+    this.dataService.getExpenses().subscribe(expense => {
+      console.log(expense);
+      this.expenses = expense;
+
+       //get sums
+    for (var i = 0; i < this.expenses.length; i++){
+      if (this.expenses[i].category == 'Groceries'){
+        this.grocSum += this.expenses[i].amount;
+      }
+      else if (this.expenses[i].category == 'Utilities'){
+        this.utilSum += this.expenses[i].amount;
+      }
+      else if (this.expenses[i].category == 'Entertainment'){
+        this.enteSum += this.expenses[i].amount;
+      }
+      else if (this.expenses[i].category == 'Rent'){
+        this.rentSum += this.expenses[i].amount;
+      }
+      else if (this.expenses[i].category == 'Clothing'){
+        this.clotSum += this.expenses[i].amount;
+      }
+      else if (this.expenses[i].category == 'Health'){
+        this.healSum += this.expenses[i].amount;
+      }
+    }
+
+    this.barChartData = [
+      {data: [this.grocSum, this.utilSum, this.enteSum, this.rentSum, this.clotSum, this.healSum], label: 'All time Expenses'},
+      
+    ]
+
+      
+    })
+
+     
+
+    
+
+
     
   }
 
