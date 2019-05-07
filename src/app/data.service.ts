126b748653afce2018f72675df4df0520685e7a6
keenanlk <keenanlkaufman@gmail.com>
2019-05-06 20:55:04 -0500

    input add record to firebase

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Users } from './models/user';
 import { Observable } from 'rxjs';
+import { Expenses } from './models/user';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  dataCollection: AngularFirestoreCollection<Users>;
+  dataCollection: AngularFirestoreCollection<Expenses>;
   expense: Observable<any[]>;
 
   constructor(public afs: AngularFirestore) {
+
+    this.dataCollection = this.afs.collection('Users/1/Expenses');
+
     this.expense = this.afs.collection('/Users/1/Expenses').valueChanges();
     console.log(this.expense);
 
@@ -19,6 +23,10 @@ export class DataService {
 
   getExpenses(){
     return this.expense;
+  }
+
+  addExpense(exp: Expenses){
+    this.dataCollection.add(exp);
   }
 }
 
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { USERS } from '../mock-users';
 import { Users } from '../Users';
 //import { Users } from '../Users';
+import { Expenses } from '../models/user';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-input',
@@ -9,15 +11,32 @@ import { Users } from '../Users';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-  
+  date: string;
+  amount: number;
+  description: string;
+  category: string;
+  arr: Expenses[] = [];
 
-  constructor() { }
+  data: Expenses = {
+    date: "",
+    description: '',
+    amount: 0,
+    category: ''
+
+  }
+
+  constructor(private dataService : DataService) { 
+    console.log(this.date);
+    
+  }
 
   ngOnInit() {
   }
 
   addExpense() {
+    //console.log(this.date.toString());
     
+    this.dataService.addExpense(this.data);
   }
 
 }
